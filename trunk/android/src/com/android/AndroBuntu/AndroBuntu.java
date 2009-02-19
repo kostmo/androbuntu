0015ae9b7a9a17eac51458310f1bb98868400cd7
kostmo <kostmo@8f90593e-f74e-11dd-ada4-5153b8187bf2>
2009-02-19 00:26:50 +0000

    fixed nullptrexception

--- trunk/android/src/com/android/AndroBuntu/AndroBuntu.java
+++ trunk/android/src/com/android/AndroBuntu/AndroBuntu.java
@@ -10,6 +10,7 @@ import java.net.Socket;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -20,23 +21,23 @@ import android.widget.Toast;
 
 import com.android.AndroBuntu.R;
 
-public class AndroBuntu extends Activity {
+public class AndroBuntu extends Activity implements View.OnClickListener {
 
-	public TextView tv;
-	public EditText et;
+	private EditText et;
 
    @Override
    public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        TextView tv = new TextView(this);
-       EditText et = new EditText(this);
+       et = new EditText(this);
        
        tv.setText( R.string.goodbye );
        
        et.setText( "192.168.0.9" );
        
        Button button = new Button(this);
-       button.setOnClickListener(mCorkyListener);
+
+       button.setOnClickListener(this);
        button.setText("booya!");
 
 
@@ -58,57 +59,33 @@ public class AndroBuntu extends Activity {
 	   
 	   	String message = "";
 	    try {
-	
-	    	Log.d("fark", "initiating");
-	    	
-//		  	tv.setText( R.string.hello );	// FIXME: This crashes!
-	    	
 
-	    	InetAddress addr = InetAddress.getByName("192.168.0.9");
-//	    	InetAddress addr = InetAddress.getByName( et.getText().toString() );	// FIXME: This ALSO crashes!
-	    	
-	    	Log.d("fark", "got address");
+	    	String farkwad = et.getText().toString();
+	    	InetAddress addr = InetAddress.getByName( farkwad );
 	    	Socket MyClient = new Socket(addr, 46645);
-	    	Log.d("fark", "connected to socket");
-	
+	    	
 	        try {
-		    	Log.d("fark", "About to instantiate buffered writer...");
-	        	BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(MyClient.getOutputStream()));
-		    	Log.d("fark", "instantiated buffered writer");
-	        	
-		    	
-		    	Log.d("fark", "About to instantiate buffered reader...");
+	        	BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(MyClient.getOutputStream()));   
 		        BufferedReader rd = new BufferedReader(new InputStreamReader(MyClient.getInputStream()));
-		    	Log.d("fark", "instantiated buffered reader");
-
 		    	
 	            wr.write("XF86AudioMute");
-		    	Log.d("fark", "wrote string");
 	            wr.flush();
-		    	Log.d("fark", "flushed string");
-		    	
-		    	
+	    	
 		    	
 		        String str;
-		        while ((str = rd.readLine()) != null) {
-			    	Log.d("fark", "read some from the buffered reader");
+		        while ((str = rd.readLine()) != null)
 		        	message += str;
-		        }
 
-		        rd.close();
-		    	Log.d("fark", "cosed buffered reader");	
 
-		    	
-		    	
+		        rd.close();	
 				wr.close();
-		    	Log.d("fark", "cosed buffered writer");
+				
 	        } catch (IOException e) {
-		    	Log.d("fark", "The buffered reader failed somehow...");
+		    	Log.d("fark", "The buffered reader/writer failed somehow...");
 	        }
 	
-	
 			MyClient.close();
-	    	Log.d("fark", "cosed the socket entirely");
+			
 	    }
 	    catch (IOException e) {
 	        System.out.println(e);
@@ -116,13 +93,11 @@ public class AndroBuntu extends Activity {
 	    return message;
    }
    
-   private View.OnClickListener mCorkyListener = new View.OnClickListener() {
-	    public void onClick(View v) {
-	    	
-
-	    	String message = send_message();
-	    	Toast.makeText(AndroBuntu.this, message, Toast.LENGTH_SHORT).show();
-	    }
-	};
+   
+   public void onClick(View v) {
+	
+	   String reply = send_message();
+	   Toast.makeText(AndroBuntu.this, reply, Toast.LENGTH_SHORT).show();
+   }
 
 }
