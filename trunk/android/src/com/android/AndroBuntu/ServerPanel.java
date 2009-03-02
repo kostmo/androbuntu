660f699673db1fbcef1d597889dad99cf4d4ea8a
kostmo <kostmo@8f90593e-f74e-11dd-ada4-5153b8187bf2>
2009-03-02 07:51:35 +0000

    added XML preferences panel template

--- trunk/android/src/com/android/AndroBuntu/ServerPanel.java
+++ trunk/android/src/com/android/AndroBuntu/ServerPanel.java
@@ -1,16 +1,47 @@
 package com.android.AndroBuntu;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 
 public class ServerPanel extends PreferenceActivity {
 
+		private EditText et;
 	
 	   @Override
 	   public void onCreate(Bundle savedInstanceState) {
 		   super.onCreate(savedInstanceState);
 
+		   
+	       /*
+	       TextView tv = new TextView(this);
+	       et = new EditText(this);
+	       tv.setText( R.string.goodbye );
+	       et.setText( "192.168.0.9" );
+	       */
+//	       String hostname = et.getText().toString();
+
+	       
+	       addPreferencesFromResource(R.xml.preferences );
+
+	       
+/*
+	       LinearLayout myfoo = new LinearLayout(this);
+	       myfoo.setOrientation(LinearLayout.VERTICAL);
+	       
+	       
+	        myfoo.addView(tv);
+	        myfoo.addView(et);
+	       
+	       
+	       setContentView(myfoo);
+	       
+	       
+	       */
 	   }
 }
