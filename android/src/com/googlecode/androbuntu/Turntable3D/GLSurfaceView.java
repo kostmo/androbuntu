254ce88d50e3f1dec9e199402939382a44c880cf
kostmo <kostmo@8f90593e-f74e-11dd-ada4-5153b8187bf2>
2011-02-21 02:22:44 +0000

    reorganization

--- android/src/com/googlecode/androbuntu/Turntable3D/GLSurfaceView.java
+++ android/src/com/googlecode/androbuntu/Turntable3D/GLSurfaceView.java
@@ -1,27 +1,4 @@
-/*
- * Copyright (C) 2008 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-//package com.example.android.apis.graphics;
-package com.googlecode.Turntable3D;
-
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+package com.googlecode.androbuntu.Turntable3D;
 
 import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
@@ -34,6 +11,12 @@ import javax.microedition.khronos.egl.EGLDisplay;
 import javax.microedition.khronos.egl.EGLSurface;
 import javax.microedition.khronos.opengles.GL;
 import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 /**
  * An implementation of SurfaceView that uses the dedicated surface for
--- android/src/com/googlecode/androbuntu/Turntable3D/UbuntuLogoRenderer.java
+++ android/src/com/googlecode/androbuntu/Turntable3D/UbuntuLogoRenderer.java
@@ -1,28 +1,4 @@
-/*
- * Copyright (C) 2008 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.android.Turntable3D;
-
-import android.content.Context;
-import android.opengl.GLU;
-import android.os.SystemClock;
-import android.util.Log;
-
-import com.android.Turntable3D.GLSurfaceView;
-
+package com.googlecode.androbuntu.Turntable3D;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -32,8 +8,10 @@ import java.util.Random;
 import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.opengles.GL10;
 
-
-import com.android.Turntable3D.Projector;
+import android.content.Context;
+import android.opengl.GLU;
+import android.os.SystemClock;
+import android.util.Log;
 
 
 class Point2d {
