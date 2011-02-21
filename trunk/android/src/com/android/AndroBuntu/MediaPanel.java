b16b314454c8c21772acfe0a120ce62ff7f9b268
kostmo <kostmo@8f90593e-f74e-11dd-ada4-5153b8187bf2>
2011-02-21 02:03:04 +0000

    Added 'launch at startup' server option

--- trunk/android/src/com/android/AndroBuntu/MediaPanel.java
+++ trunk/android/src/com/android/AndroBuntu/MediaPanel.java
@@ -16,75 +16,75 @@ import android.widget.Toast;
 
 public class MediaPanel extends Activity implements View.OnClickListener {
 
-		private SocketMonitor service_binder;
-		private MediaController mc;
-		
-	   @Override
-	   public void onCreate(Bundle savedInstanceState) {
-		   super.onCreate(savedInstanceState);
+	private SocketMonitor service_binder;
+	private MediaController mc;
 
-   
-	       Intent i = new Intent();
-	       i.setClass(MediaPanel.this, SocketMonitor.class);
-	       boolean connect_successful = bindService(i, my_relay_service, BIND_AUTO_CREATE);
-	       
-
-	       
-
-	       setContentView(R.layout.media);
-
-	       ImageButton mute_button = (ImageButton) findViewById(R.id.mute_button);
-	       mute_button.setOnClickListener(this);
-//	       button.setText("Mute");
-	       
-	       ImageButton voldown_button = (ImageButton) findViewById(R.id.voldown_button);
-	        voldown_button.setOnClickListener(voldown_listener);
-//	        voldown_button.setText("VolDown");
-
-		    ImageButton volup_button = (ImageButton) findViewById(R.id.volup_button);
-	        volup_button.setOnClickListener(volup_listener);
-//	        volup_button.setText("VolUp");
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
 
-	        View v = (View) findViewById(R.layout.media);
-//	        View v = (View) findViewById(R.id.media_player_button);
-	        
-	        mc = new MediaController(this);
-	        mc.setMediaPlayer(player_interface);
-	        mc.setEnabled(true);
-	        mc.setAnchorView(v);
-	        
-	        
-	        
-	        
-
-	        Button media_player_button = (Button) findViewById(R.id.media_player_button);
-	        media_player_button.setOnClickListener(media_button_listener);
-	   }
-
-	   
-	   
-	   private View.OnClickListener media_button_listener = new View.OnClickListener() {
-		    public void onClick(View v) {
-		    	
-		    	
-		    	Log.d("forker", "Can haz media player?");
-
-		    	
-		    	mc.requestFocus();
-		    	mc.show(5);
-		    	
-		    	Log.d("forker", "Getting percentage...");
-    	
-		    	
-		    	String[] reply = service_binder.send_message("exaile_query");    	
-				Toast.makeText(MediaPanel.this, reply[0], Toast.LENGTH_SHORT).show();
-		    }
-		};
-	   
+		Intent i = new Intent();
+		i.setClass(MediaPanel.this, SocketMonitor.class);
+		boolean connect_successful = bindService(i, my_relay_service, BIND_AUTO_CREATE);
 
 
-	   private MediaController.MediaPlayerControl player_interface = new MediaController.MediaPlayerControl() {
+
+
+		setContentView(R.layout.media);
+
+		ImageButton mute_button = (ImageButton) findViewById(R.id.mute_button);
+		mute_button.setOnClickListener(this);
+		//	       button.setText("Mute");
+
+		ImageButton voldown_button = (ImageButton) findViewById(R.id.voldown_button);
+		voldown_button.setOnClickListener(voldown_listener);
+		//	        voldown_button.setText("VolDown");
+
+		ImageButton volup_button = (ImageButton) findViewById(R.id.volup_button);
+		volup_button.setOnClickListener(volup_listener);
+		//	        volup_button.setText("VolUp");
+
+
+		View v = (View) findViewById(R.layout.media);
+		//	        View v = (View) findViewById(R.id.media_player_button);
+
+		mc = new MediaController(this);
+		mc.setMediaPlayer(player_interface);
+		mc.setEnabled(true);
+		mc.setAnchorView(v);
+
+
+
+
+
+		Button media_player_button = (Button) findViewById(R.id.media_player_button);
+		media_player_button.setOnClickListener(media_button_listener);
+	}
+
+
+
+	private View.OnClickListener media_button_listener = new View.OnClickListener() {
+		public void onClick(View v) {
+
+
+			Log.d("forker", "Can haz media player?");
+
+
+			mc.requestFocus();
+			mc.show(5);
+
+			Log.d("forker", "Getting percentage...");
+
+
+			String[] reply = service_binder.send_message("exaile_query");    	
+			Toast.makeText(MediaPanel.this, reply[0], Toast.LENGTH_SHORT).show();
+		}
+	};
+
+
+
+	private MediaController.MediaPlayerControl player_interface = new MediaController.MediaPlayerControl() {
 
 		public int getBufferPercentage() {
 			return 75;
@@ -110,54 +110,72 @@ public class MediaPanel extends Activity implements View.OnClickListener {
 
 		public void start() {
 		}
-	   };
-	   
-	   private ServiceConnection my_relay_service = new ServiceConnection() {
-	       public void onServiceConnected(ComponentName className, IBinder service) {
+
+		@Override
+		public boolean canPause() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean canSeekBackward() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean canSeekForward() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+	};
+
+	private ServiceConnection my_relay_service = new ServiceConnection() {
+		public void onServiceConnected(ComponentName className, IBinder service) {
 
 
-	    	   service_binder = ((SocketMonitor.LocalBinder) service).getService();
-	    	   
-	    	   Log.d("forker", "Successfully connected to SocketMonitor service.");
-	       }
+			service_binder = ((SocketMonitor.LocalBinder) service).getService();
 
-	       public void onServiceDisconnected(ComponentName componentName) {
+			Log.d("forker", "Successfully connected to SocketMonitor service.");
+		}
+
+		public void onServiceDisconnected(ComponentName componentName) {
 
 
-	    	   Log.d("forker", "SocketMonitor service disconnected.");
-	       }
-	   };
+			Log.d("forker", "SocketMonitor service disconnected.");
+		}
+	};
 
-	   
-	   
-	   
-		
-	   private View.OnClickListener voldown_listener = new View.OnClickListener() {
-		    public void onClick(View v) {
-		    	String[] reply = service_binder.send_message("XF86AudioLowerVolume");    	
-			   Toast.makeText(MediaPanel.this, reply[0], Toast.LENGTH_SHORT).show();
-		    }
-		};
 
-	   private View.OnClickListener volup_listener = new View.OnClickListener() {
-		    public void onClick(View v) {
-		    	
-		    	String reply[] = service_binder.send_message("XF86AudioRaiseVolume");    	
-			   Toast.makeText(MediaPanel.this, reply[0], Toast.LENGTH_SHORT).show();
-		    }
-		};
-	       
-	   public void onClick(View v) {
 
-		   String reply[] = service_binder.send_message("XF86AudioMute");
-		   Toast.makeText(MediaPanel.this, reply[0], Toast.LENGTH_SHORT).show();
-	   }
-	   
-	   
-	    @Override
-	    protected void onDestroy() {
-	        super.onDestroy();
 
-	    	unbindService(my_relay_service);
-	    }
+
+	private View.OnClickListener voldown_listener = new View.OnClickListener() {
+		public void onClick(View v) {
+			String[] reply = service_binder.send_message("XF86AudioLowerVolume");    	
+			Toast.makeText(MediaPanel.this, reply[0], Toast.LENGTH_SHORT).show();
+		}
+	};
+
+	private View.OnClickListener volup_listener = new View.OnClickListener() {
+		public void onClick(View v) {
+
+			String reply[] = service_binder.send_message("XF86AudioRaiseVolume");    	
+			Toast.makeText(MediaPanel.this, reply[0], Toast.LENGTH_SHORT).show();
+		}
+	};
+
+	public void onClick(View v) {
+
+		String reply[] = service_binder.send_message("XF86AudioMute");
+		Toast.makeText(MediaPanel.this, reply[0], Toast.LENGTH_SHORT).show();
+	}
+
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		unbindService(my_relay_service);
+	}
 }
--- trunk/android/src/com/android/Turntable3D/UbuntuLogoRenderer.java
+++ trunk/android/src/com/android/Turntable3D/UbuntuLogoRenderer.java
@@ -48,6 +48,7 @@ class Point2d {
 
 public class UbuntuLogoRenderer implements GLSurfaceView.Renderer{
 
+	
 	private TurntableWidget parent;
 	
 	float eye_z, frust_near, frust_far;
@@ -56,6 +57,64 @@ public class UbuntuLogoRenderer implements GLSurfaceView.Renderer{
 
 	private long last_frame_time;
 	private float last_angular_position;	// On a scale from 0 to 1
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    private static final float[] _tempGluUnProjectData = new float[40]; 
+    private static final int     _temp_m   = 0; 
+    private static final int     _temp_A   = 16; 
+    private static final int     _temp_in  = 32; 
+    private static final int     _temp_out = 36; 
+    public static int gluUnProject(float winx, float winy, float winz, 
+    		float model[], int offsetM, 
+    		float proj[], int offsetP, 
+    		int viewport[], int offsetV, 
+    		float[] xyz, int offset) 
+    { 
+    	/* Transformation matrices */ 
+    	//   float[] m = new float[16], A = new float[16]; 
+    	//   float[] in = new float[4], out = new float[4]; 
+    	/* Normalize between -1 and 1 */ 
+    	_tempGluUnProjectData[_temp_in]   = (winx - viewport[offsetV]) * 
+    	2f / viewport[offsetV+2] - 1.0f; 
+    	_tempGluUnProjectData[_temp_in+1] = (winy - viewport[offsetV+1]) * 
+    	2f / viewport[offsetV+3] - 1.0f; 
+    	_tempGluUnProjectData[_temp_in+2] = 2f * winz - 1.0f; 
+    	_tempGluUnProjectData[_temp_in+3] = 1.0f; 
+    	/* Get the inverse */ 
+    	android.opengl.Matrix.multiplyMM(_tempGluUnProjectData, _temp_A, 
+    			proj, offsetP, model, offsetM); 
+    	android.opengl.Matrix.invertM(_tempGluUnProjectData, _temp_m, 
+    			_tempGluUnProjectData, _temp_A); 
+    	android.opengl.Matrix.multiplyMV(_tempGluUnProjectData, _temp_out, 
+    			_tempGluUnProjectData, _temp_m, 
+    			_tempGluUnProjectData, _temp_in); 
+    	if (_tempGluUnProjectData[_temp_out+3] == 0.0) 
+    		return GL10.GL_FALSE; 
+    	xyz[offset]  =  _tempGluUnProjectData[_temp_out  ] / 
+    	_tempGluUnProjectData[_temp_out+3]; 
+    	xyz[offset+1] = _tempGluUnProjectData[_temp_out+1] / 
+    	_tempGluUnProjectData[_temp_out+3]; 
+    	xyz[offset+2] = _tempGluUnProjectData[_temp_out+2] / 
+    	_tempGluUnProjectData[_temp_out+3]; 
+    	return GL10.GL_TRUE; 
+    } 
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
     public UbuntuLogoRenderer(Context context) {
@@ -166,7 +225,7 @@ public class UbuntuLogoRenderer implements GLSurfaceView.Renderer{
 
 //        Log.d("bork0", "Desired window coords: (" + String.valueOf(desired_reprojection_coords[0]) + ", " + String.valueOf(desired_reprojection_coords[1]) + ")");
 		
-		GLU.gluUnProject(desired_reprojection_coords[0], desired_reprojection_coords[1], desired_reprojection_coords[2], model_mat, 0, proj_mat, 0, viewport, 0, obj, 0);
+		gluUnProject(desired_reprojection_coords[0], desired_reprojection_coords[1], desired_reprojection_coords[2], model_mat, 0, proj_mat, 0, viewport, 0, obj, 0);
 
 
 //        Log.d("bork1", "Pos: (" + String.valueOf(obj[0]) + ", " + String.valueOf(obj[1]) + ", " + String.valueOf(obj[2]) + ")");
