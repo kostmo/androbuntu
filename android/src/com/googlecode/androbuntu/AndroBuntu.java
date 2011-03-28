c0f54b3faeeb946540bee92f1b13684ac1c5ec37
kostmo <kostmo@8f90593e-f74e-11dd-ada4-5153b8187bf2>
2011-03-28 04:13:45 +0000

    Added WOL capability

--- android/src/com/googlecode/androbuntu/AndroBuntu.java
+++ android/src/com/googlecode/androbuntu/AndroBuntu.java
@@ -1,5 +1,8 @@
 package com.googlecode.androbuntu;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +17,7 @@ import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,6 +31,8 @@ import com.googlecode.androbuntu.services.ServiceSocketMonitor;
 
 public class AndroBuntu extends Activity implements View.OnClickListener {
 
+	private static String TAG = "AndroBuntu";
+	
 
 	private ServiceSocketMonitor service_binder;
 
@@ -86,9 +92,96 @@ public class AndroBuntu extends Activity implements View.OnClickListener {
 
 		Button media_button = (Button) findViewById(R.id.media_button);
 		media_button.setOnClickListener(media_button_listener);
+		
+
+		Button wol_button = (Button) findViewById(R.id.wol_button);
+		wol_button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				Log.d(TAG, "Sending wake packet...");
+				String[] args = new String[] {"192.168.0.80", "90:e6:ba:5d:16:4e"};
+				WakeUpPC(args);
+				
+				Log.d(TAG, "Wake packet sent!");
+				
+//				send_lights_out( AndroBuntu.this, messages, service_binder );
+			}
+		});
 	}
 
-
+	
+	
+	
+	
+	 
+    public static final int PORT = 9;    
+    
+    public static void WakeUpPC(String[] args) {
+        
+              
+        String ipStr = args[0];
+        String macStr = args[1];
+        
+        try {
+            byte[] macBytes = getMacBytes(macStr);
+            byte[] bytes = new byte[6 + 16 * macBytes.length];
+            for (int i = 0; i < 6; i++) {
+                bytes[i] = (byte) 0xff;
+            }
+            for (int i = 6; i < bytes.length; i += macBytes.length) {
+                System.arraycopy(macBytes, 0, bytes, i, macBytes.length);
+            }
+            
+            InetAddress address = InetAddress.getByName(ipStr);
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, PORT);
+            DatagramSocket socket = new DatagramSocket();
+            socket.send(packet);
+            socket.close();
+            
+                    }
+        catch (Exception e) {
+            //System.out.println("Failed to send Wake-on-LAN packet: + e");
+            System.exit(1); 
+           //NOTE exception or error here will close Android-application 
+        }
+        
+    }
+    
+    private static byte[] getMacBytes(String macStr) throws IllegalArgumentException {
+        byte[] bytes = new byte[6];
+        String[] hex = macStr.split("(\\:|\\-)");
+        if (hex.length != 6) {
+            throw new IllegalArgumentException("Invalid MAC address."); 
+      //NOTE also this error will close Android-application
+        }
+        try {
+            for (int i = 0; i < 6; i++) {
+                bytes[i] = (byte) Integer.parseInt(hex[i], 16);
+            }
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid hex digit in MAC address."); 
+       //NOTE also this error will close Android-application
+        }
+        return bytes;
+    }
+    
+   
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	private ServiceConnection my_relay_service = new ServiceConnection() {
 		public void onServiceConnected(ComponentName className, IBinder service) {
 			service_binder = ((ServiceSocketMonitor.LocalBinder) service).getService();
